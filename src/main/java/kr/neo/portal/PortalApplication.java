261333b119d37354f7805dfeaaed24064e9f8d69
jihoon <gloryhoon@gmail.com>
2022-04-16 16:36:38 +0900

    commit test 하하하

--- src/main/java/kr/neo/portal/PortalApplication.java
+++ src/main/java/kr/neo/portal/PortalApplication.java
@@ -8,6 +8,8 @@ public class PortalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PortalApplication.class, args);
+
+        System.out.println("하하하");
     }
 
 }
