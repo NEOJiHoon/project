e13be93bfe5ccbc7978b93fe14f106109c69ba90
jihoon <gloryhoon@gmail.com>
2022-04-30 16:33:08 +0900

    commit test 코드

---
+++ src/test/java/kr/neo/portal/study/NumberTest.java
@@ -0,0 +1,16 @@
+package kr.neo.portal.study;
+
+import org.junit.jupiter.api.Test;
+
+public class NumberTest {
+
+    @Test
+    public void test() {
+        System.out.println("테스트");
+
+        int a = 100;
+        int b = 2;
+
+        System.out.println("a / b = " + a / b);
+    }
+}
