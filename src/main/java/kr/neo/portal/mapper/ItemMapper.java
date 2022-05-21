b25072b281ef3f875c4d893e3e9247b603986b3c
JINAM KANG <jnkang@welcomepayments.co.kr>
2022-05-21 16:41:13 +0900

    설명 추가

--- src/main/java/kr/neo/portal/mapper/ItemMapper.java
+++ src/main/java/kr/neo/portal/mapper/ItemMapper.java
@@ -5,13 +5,13 @@ import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
-@Mapper // 스프링컨테이너의 빈(bean)으로 등록됨
+@Mapper // 스프링컨테이너의 스프링빈(bean)으로 등록됨, 데이터베이스에 조회/등록/수정/삭제(CRUD) 를 처리함
 public interface ItemMapper {
-    int insertItem(Item item);
+    int insertItem(Item item);// DB 에 아이템 등록
 
-    int deleteItem(int itemId);
+    int deleteItem(int itemId);// DB 에 아이템을 삭제
 
-    int updateItem(Item item);
+    int updateItem(Item item);// DB 에 아이템을 수정
 
-    List<Item> selectItems();
+    List<Item> selectItems();// DB 에서 데이터를 조회
 }
--- src/main/java/kr/neo/portal/model/Item.java
+++ src/main/java/kr/neo/portal/model/Item.java
@@ -1,9 +1,9 @@
 package kr.neo.portal.model;
 
-public class Item {
-    private int itemId;
-    private String itemName;
-    private int itemPrice;
+public class Item { // 데이터베이스 테이블명
+    private int itemId;// 데이터베이스 필드명의 카멜케이스 DB: item_id
+    private String itemName;// DB: item_name
+    private int itemPrice;// DB: item_price
 
     public int getItemId() {
         return itemId;
