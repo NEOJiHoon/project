049a2590abcace4718f37df85bf8cea0de86e68d
Ji Hoon <gloryhoon@gmail.com>
2022-06-12 16:35:27 +0900

    비동기 등록기능 추가

--- src/main/resources/static/item.js
+++ src/main/resources/static/item.js
@@ -83,3 +83,21 @@ function deleteTargetItem() {
     console.log('진짜 삭제', deleteTargetId);
     deleteItem(deleteTargetId);
 }
+
+function createItem() {
+    console.log('등록합니다.', $("#input-id").val(), $("#input-name").val(), $("#input-price").val());
+
+    $.ajax({
+        type : "GET",            // HTTP method type(GET, POST) 형식이다.
+        url : "/item/add/" + $("#input-id").val() + "/" + $("#input-name").val() + "/" + $("#input-price").val(),      // 컨트롤러에서 대기중인 URL 주소이다.
+        // data : params,            // Json 형식의 데이터이다.
+        success : function(data){ // 비동기통신의 성공일경우 success콜백으로 들어옵니다. 'res'는 응답받은 데이터이다.
+
+            alert('등록되었습니다.', 'success');
+            getItemList();
+        },
+        error : function(XMLHttpRequest, textStatus, errorThrown){ // 비동기 통신이 실패할경우 error 콜백으로 들어옵니다.
+            alert("통신 실패.")
+        }
+    });
+}
